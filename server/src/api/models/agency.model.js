f1cbd352b2b6c6afa892a42a59c9a1a48cb3f03f
ayaaas <125189175+ayaaas@users.noreply.github.com>
2024-03-15 15:10:00 +0000

    Update agency.model.js

--- server/src/api/models/agency.model.js
+++ server/src/api/models/agency.model.js
@@ -15,11 +15,27 @@ var agency = new mongoose.Schema({
     trim: true,
   },
 
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  portfolio: {
+    type: mongoose.Schema.Types.objectId,
+    ref: "Portfolio",
+    required: true,
+  },
+},
+  {
+    timestamp: true,
+  });
 
-});
-
-
-
-const Agency = mongoose.model("Agency",agency );
-module.exports = Agency ;
+const Agency = mongoose.model("Agency", agency);
+module.exports = Agency;
 
