c3ca3fbc8b99d6725425959d453659a1ff995c5f
Youness <85195141+YounessErrid@users.noreply.github.com>
2024-04-18 23:44:45 +0100

    fix conflicts on agency and portfolio controllers

--- server/src/api/controllers/agency.controller.js
+++ server/src/api/controllers/agency.controller.js
@@ -54,6 +54,7 @@ const login = (req, res) => {
     message: "Successfully logged in",
   });
 };
+
 const destroy = async (req, res) => {
   req.logout(function (err) {
     if (err) {
@@ -66,147 +67,9 @@ const destroy = async (req, res) => {
   });
 };
 
-// const create = async (req, res) => {
-//   const { email, password, fullname, phone } = req.body;
-
-//   try {
-//     if (!email || !password || !fullname || !phone) {
-//       return res.status(400).json({
-//         error: "Agency creation failed: Missing required information!",
-//       });
-//     }
-
-//     const newAgency = new Agency({ email, password, fullname, phone });
-//     await newAgency.save();
-
-//     res.status(201).json({
-//       success: true,
-//       message: "Agency created successfully",
-//     });
-//   } catch (error) {
-//     return res
-//       .status(500)
-//       .json([
-//         { error: "Internal server error" },
-//         { message: `Error creating Agency: ${error.message}` },
-//       ]);
-//   }
-// };
-
-// const findOne = async (req, res) => {
-//   const { id } = req.params;
-
-  try {
-    const Agency = await Agency.findById(id);
-
-    if (Agency) {
-      return res.status(200).json(Agency);
-    } else {
-      return res.status(404).json({ error: "Agency not found!" });
-    }
-  } catch (error) {
-    return res
-      .status(500)
-      .json([
-        { error: "Internal server error" },
-        { message: `Error finding Agency: ${error.message}` },
-      ]);
-  }
-};
-
-const viewAll = async (req, res) => {
-  try {
-    const Agencys = await Agency.find();
-
-    if (Agencys.length > 0) {
-      return res.status(200).json(Agencys);
-    } else {
-      return res.status(404)
-      .json({ error: "No Agencys found!" });
-    }
-  } catch (error) {
-    return res
-      .status(500)
-      .json([
-        { error: "Internal server error" },
-        { message: `Error retrieving Agencys: ${error.message}` },
-      ]);
-  }
-};
-
-const update = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { createdAt, email, ...newAgencyData } = req.body; // Exclude createdAt from newPostData
-
-    if (!id || !newAgencyData) {
-      return res
-        .status(400)
-        .json({ error: "Agency update failed: Missing required fields!" });
-    }
-
-    // Update the Agency with the given ID
-    const updatedAgency = await Agency.findByIdAndUpdate(
-      id,
-      {
-        ...newAgencyData,
-        $set: { updatedAt: new Date() },
-      },
-      { new: true }
-    );
-
-    if (!updatedAgency) {
-      return res.status(404).json({ error: "Agency not found!" });
-    }
-
-    return res.status(200).json(updatedAgency);
-  } catch (error) {
-    return res
-      .status(500)
-      .json([
-        { error: "Internal server error" },
-        { message: `Error updating Agency: ${error.message}` },
-      ]);
-  }
-};
-
-// const remove = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-
-    if (!id) {
-      return res.status(400).json({
-        error: "Agency deletion failed: Missing required information!",
-      });
-    }
-
-    const deletedAgency = await Agency.findByIdAndDelete(id);
-
-    if (!deletedAgency) {
-      return res.status(404).json({ error: "Agency not found!" });
-    }
-
-    return res.status(200).json({
-      success: true,
-      message: "Agency deleted successfully",
-    });
-  } catch (error) {
-    return res
-      .status(500)
-      .json([
-        { error: "Internal server error" },
-        { message: `Error deleting Agency: ${error.message}` },
-      ]);
-  }
-};
 
 module.exports = {
   register,
   login,
   destroy,
-  // create,
-  findOne,
-  viewAll,
-  update,
-  remove,
 };
--- server/src/api/controllers/portfolio.controller.js
+++ server/src/api/controllers/portfolio.controller.js
@@ -1,22 +1,21 @@
 const Portfolio = require("../models/portfolio.model");
 
-
 const create = async (req, res) => {
-  const { description} = req.body;
-   const logo = req.files["logo"][0].path;
-   const cover = req.files["cover"][0].path;
+  const { description } = req.body;
+  const logo = req.files["logo"][0].path;
+  const cover = req.files["cover"][0].path;
+
   try {
     if (!description || !logo || !cover) {
       return res.status(400).json({
         error: "Portfolio creation failed: Missing required information!",
       });
     }
-  
+
     const newPortfolio = new Portfolio({
       description,
       logo,
       cover,
-     
     });
     await newPortfolio.save();
     return res.status(201).json({
@@ -50,42 +49,36 @@ const viewAll = async (req, res) => {
       ]);
   }
 };
-// 
+
 const update = async (req, res) => {
   const { id } = req.params;
-  const { newPortfolio} = req.body;
+  const { newPortfolio } = req.body;
   const logo = req.files["logo"][0].path;
   const cover = req.files["cover"][0].path;
- 
 
   if (!newPortfolio) {
-      return res.status(400).json({
-          error: "Missing required information for update!",
-      });
+    return res.status(400).json({
+      error: "Missing required information for update!",
+    });
   }
   try {
- 
-      const updatedPortfolio = await Portfolio.findByIdAndUpdate(
-          id,
-          { newPortfolio, cover, logo},
-          { new: true }
-      );
+    const updatedPortfolio = await Portfolio.findByIdAndUpdate(
+      id,
+      { newPortfolio, cover, logo },
+      { new: true }
+    );
 
-      if (!updatedPortfolio) {
-          return res.status(404).json({ error: "Portfolio not found!" });
-      }
-      return res.status(200).json(updatedPortfolio);
-
+    if (!updatedPortfolio) {
+      return res.status(404).json({ error: "Portfolio not found!" });
+    }
+    return res.status(200).json(updatedPortfolio);
   } catch (error) {
-  
-      console.error(`Error updating portfolio with ID ${id}:`, error);
-      return res.status(500).json({
-          error: "Internal server error",
-          message: `Error updating portfolio: ${error.message}`,
-      });
+    console.error(`Error updating portfolio with ID ${id}:`, error);
+    return res.status(500).json({
+      error: "Internal server error",
+      message: `Error updating portfolio: ${error.message}`,
+    });
   }
-
-
 };
 
 const remove = async (req, res) => {
@@ -93,21 +86,19 @@ const remove = async (req, res) => {
     const { id } = req.params;
     const deletedPortfolio = await Portfolio.findByIdAndDelete(id);
     if (!deletedPortfolio) {
-                return res.status(404).json({ error: "Portfolio not found!" });
-            }
-            return res.status(200).json({
-                message: "Portfolio deleted successfully!",
-                portfolio: deletedPortfolio
-            });
-            
-        } catch (error) {
-           
-            console.error(`Error deleting portfolio with ID ${id}:`, error);
-            return res.status(500).json({
-                error: "Internal server error",
-                message: `Error deleting portfolio: ${error.message}`
-            });
-        }
+      return res.status(404).json({ error: "Portfolio not found!" });
+    }
+    return res.status(200).json({
+      message: "Portfolio deleted successfully!",
+      portfolio: deletedPortfolio,
+    });
+  } catch (error) {
+    console.error(`Error deleting portfolio with ID ${id}:`, error);
+    return res.status(500).json({
+      error: "Internal server error",
+      message: `Error deleting portfolio: ${error.message}`,
+    });
+  }
 };
 
 const findOne = async (req, res) => {
@@ -124,15 +115,12 @@ const findOne = async (req, res) => {
     console.error("Error finding portfolio:", error);
     // Return a JSON response with a status code of 500
     return res.status(500).json({
-        error: "Internal server error",
-        message: error.message, // Include the error message for debugging
-        success: false
+      error: "Internal server error",
+      message: error.message, // Include the error message for debugging
+      success: false,
     });
-}
-
+  }
 };
-
-
 
 module.exports = {
   create,
