286fdc5f7bf3a0e20a3ee797421a30f04ddaa81d
Youness <85195141+YounessErrid@users.noreply.github.com>
2024-04-19 00:46:28 +0100

    integration of redux and setup tailwindCss

---
+++ client/src/features/userSlice.js
@@ -0,0 +1,64 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api/auth";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+  loading: false,
+  error: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // login user
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.isAuthenticated = true;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        if (action.error.message === "Rejected") {
+          state.error = "Sign up Failed! Invalid Inputs";
+        } else {
+          state.error = action.error.message;
+        }
+      })
+
+  },
+});
+
+// LOGIN USER
+export const loginUser = createAsyncThunk(
+    "user/loginUser",
+    async (userCredentials, { rejectWithValue }) => {
+      try {
+        const request = await axios.post(
+          API_URL.concat("/login"),
+          userCredentials,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true, // Send cookies with the request
+          }
+        );
+        const response = await request.data;
+        return response;
+      } catch (error) {
+        return rejectWithValue(error.message);
+      }
+    }
+  );
+
+export default userSlice.reducer;
--- server/src/api/controllers/portfolioservice.controller.js
+++ server/src/api/controllers/portfolioservice.controller.js
@@ -1,62 +1,30 @@
 const portfolioservice = require("../models/portfolioServices.model");
 
-
-//   const { name, description } = req.body;
-//   const path = req.files;
-//   let images = [];
-//   path.forEach((element) => {
-//     images.push(element.path);
-//   });
-//   try {
-//     if (!name || !description) {
-//       return res.status(400).json({
-//         message: "Please fill all the fields",
-//       });
-//     }
-//     const newPortfolioService = new portfolioservice({
-//       name,
-//       description,
-//       // portfolioId,
-//       images,
-//       // serviceId,
-//     });
-//     await newPortfolioService.save();
-//     return res.status(200).json({
-//       message: "Portfolio service created successfully",
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       error: "Internal server error",
-//       message: error.message,
-//     });
-//   }
-// };
 const create = async (req, res) => {
-  const { name, description, service} = req.body;
+  const { name, description, service } = req.body;
   const path = req.files;
   let images = [];
   path.forEach((element) => {
     images.push(element.path);
   });
-  console.log(path);
   try {
     if (!name || !description || !service) {
       return res.status(400).json({
-        error: "^portfolioService creation failed: Missing required information!",
+        error:
+          "^portfolioService creation failed: Missing required information!",
       });
     }
     const newPortfolioService = new portfolioservice({
       name,
       description,
       images,
-      service
+      service,
     });
 
     const data = await newPortfolioService.save();
     return res.status(200).json({
       message: "Portfolio service created successfully",
-      data : data,
-      
+      data: data,
     });
   } catch (error) {
     return res.status(500).json({
@@ -67,42 +35,43 @@ const create = async (req, res) => {
 };
 
 const update = async (req, res) => {
-    try {
-      const { id } = req.params;
-          const { ...newPortfolioService } = req.body;
-          console.log(req.files);
-          const files = req.files;
-          let images = [];
-          if (files){
-          files.forEach((element) => {
-            images.push(element.path);
-          })
-          }
-          
-          console.log(files);
-          console.log(images);
-      if (!id ||!newPortfolioService) {
-        return res.status(400).json({
-          error: "PortfolioService update failed: Missing required information!",
-        });
-      }
-      const updatedPortfolioService = await portfolioservice.findByIdAndUpdate(
-        id,
-        { ...newPortfolioService, images}, {new: true}
-      );
-      if (!updatedPortfolioService) {
-        return res.status(404).json({
-          message: "Portfolio service not found",
-        });
-      }
-      return res.status(200).json(updatedPortfolioService);
-    } catch (error) {
-      return res.status(500).json({
-        error: "Internal server error",
-        message: error.message,
+  try {
+    const { id } = req.params;
+    const { ...newPortfolioService } = req.body;
+    console.log(req.files);
+    const files = req.files;
+    let images = [];
+    if (files) {
+      files.forEach((element) => {
+        images.push(element.path);
       });
     }
-  };
+
+    console.log(files);
+    console.log(images);
+    if (!id || !newPortfolioService) {
+      return res.status(400).json({
+        error: "PortfolioService update failed: Missing required information!",
+      });
+    }
+    const updatedPortfolioService = await portfolioservice.findByIdAndUpdate(
+      id,
+      { ...newPortfolioService, images },
+      { new: true }
+    );
+    if (!updatedPortfolioService) {
+      return res.status(404).json({
+        message: "Portfolio service not found",
+      });
+    }
+    return res.status(200).json(updatedPortfolioService);
+  } catch (error) {
+    return res.status(500).json({
+      error: "Internal server error",
+      message: error.message,
+    });
+  }
+};
 const remove = async (req, res) => {
   try {
     const { id } = req.params;
